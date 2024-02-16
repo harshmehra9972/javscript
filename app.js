659b3d2294dd8ba2d847ac1d0d4b497bbd732f13
Full Name <harshmehra9972@gmail.com>
2024-02-16 15:52:58 +0530

    added index.html

---
+++ app.js
@@ -0,0 +1,33 @@
+let name='my name is harsh mehra'
+let age='im am 19 years old'
+let hobbies= ['sports', 'wacth movies','listen music']
+
+alert(name)
+alert(age)
+alert(hobbies)
+
+let job ={ tittle: 'devlpoer', place:'new york'}
+alert(job.place) 
+
+let agee=32
+let adult = agee -2
+
+alert(adult);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
