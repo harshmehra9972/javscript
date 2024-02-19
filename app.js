7bef3b4a346af3648a9fdd90ea0a4033b52ff937
Full Name <harshmehra9972@gmail.com>
2024-02-19 15:53:17 +0530

    added exercisse.js

--- app.js
+++ app.js
@@ -1,29 +1,72 @@
-let name='my name is harsh mehra'
-let age='im am 19 years old'
-let hobbies= ['sports', 'wacth movies','listen music']
+// variable
+// let name='my name is harsh mehra'
 
-alert(name)
-alert(age)
-alert(hobbies)
+// let age='im am 19 years old'
 
-let job ={ tittle: 'devlpoer', place:'new york'}
-alert(job.place) 
-
-let agee=32
-let adult = agee -2
-
-alert(adult);
+// let hobbies= ['sports', 'wacth movies','listen music']
 
 
+// alert(name)
+
+// alert(age)
+
+// alert(hobbies)
 
 
+// let job ={ tittle: 'devlpoer', place:'new york'}
+// alert(job.place) 
+// =========================================================//
 
+// funtion//
+// let agee=32
+// let adult = agee -2
 
+// alert(adult);
 
+// let adultyears ;
+// let agee=32 
+// function adultage() {
+//     adultyears =agee-16
+// }
+// adultage();
+// alert(adultyears); 
 
+// agee=45;
+// adultage();
+// alert(adultyears); 
+// ==========================================================//
 
+// function with return value//
+// let agee=32 
+// let totaladultYears;
+// function adultage(){
+//     return agee-18
+// }
+// totaladultYears = adultage();
+// alert(totaladultYears);
+// agee=45;
+// totaladultYears = adultage();
+// alert(totaladultYears)
 
+// ==========================================================//
+// passing data with parameter//
+// function user (fname = "harsh",lname = "mehra"){
+//     document.write (fname +" " + lname);
+//   }
 
+// user();
+
+// ===========================================================//
+// object
+
+let person = {
+    name :'max',
+    greet() {
+        alert('hello');
+    }
+};
+
+person.greet();
 
 
 
